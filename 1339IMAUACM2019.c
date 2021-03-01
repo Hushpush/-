9c6546a83ed0b3daa5652e6ddea980d4da67c1c7
Hushpush <571273394@qq.com>
2021-03-01 19:52:22 +0800

    C练习

---
+++ 1339IMAUACM2019.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int N,i;
+    while(scanf("%d",&N)!=EOF)
+    {
+        for(i=0;i<N;i++)
+            printf("IMAUACM2019\n");
+        printf("\n");
+    }
+return 0;
+}
